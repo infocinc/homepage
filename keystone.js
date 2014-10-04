08a7cd7f333593656795bd543f1141e7801f0fd5
infocinc <nicolas.dutil@infocinc.com>
2014-10-04 17:15:41 -0400

    remove post

--- keystone.js
+++ keystone.js
@@ -89,7 +89,7 @@ keystone.set('email tests', require('./routes/emails'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'posts': ['posts', 'post-categories'],
+//	'posts': ['posts', 'post-categories'],
 	'users': 'users'
 });
 
