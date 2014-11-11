ca9fd4811d10a30319126bd4c3f71ad82be67403
infocinc <nicolas.dutil@infocinc.com>
2014-11-11 00:37:26 -0500

    change production url

--- keystone.js
+++ keystone.js
@@ -110,10 +110,10 @@ keystone.set('email locals', {
 
 keystone.set('email rules', [{
 	find: '/images/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.infocinc.com/images/' : 'http://localhost:3000/images/'
+	replace: (keystone.get('env') == 'production') ? 'http://infocinc.herokuapp.com/images/' : 'http://localhost:3000/images/'
 }, {
 	find: '/keystone/',
-	replace: (keystone.get('env') == 'production') ? 'http://www.infocinc.com/keystone/' : 'http://localhost:3000/keystone/'
+	replace: (keystone.get('env') == 'production') ? 'http://infocinc.herokuapp.com/keystone/' : 'http://localhost:3000/keystone/'
 }]);
 
 // Load your project's email test routes
