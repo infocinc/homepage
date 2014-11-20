b1f87ed9f07f24154e86be7d3a1df8ed54f1883a
infocinc <nicolas.dutil@infocinc.com>
2014-11-20 16:06:22 -0500

    Add localeResource model and update

---
+++ updates/0.0.1-locales.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+
+var namespaces = [
+		'app','home','form','services',
+		'footer','portfolio','terms',
+		'contact'
+	],
+	lngs = ['fr', 'en'],
+	resources = [];
+
+
+function loadResource($ns,$lng) {
+	var data = fs.readFileSync('./locales/' + $lng +'/' + $ns + '.json', 'utf8');
+	return {
+		_id: $ns + '_' + $lng,
+		ns: $ns,
+		lng:  $lng,
+		resource: JSON.stringify(JSON.parse(data)) 
+	}
+}
+
+
+var tuples = [];
+// create tuples of ns, lng
+namespaces.forEach(function(ns) {
+	lngs.forEach(function(lng) {
+		resources.push(loadResource(ns,lng));
+	});
+});
+
+console.log(resources);
+
+exports.create = {
+	LocaleResource: resources
+};
+
+
+
