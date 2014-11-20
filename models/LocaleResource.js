5d243385ecea9006aba55e1f85d8b81a8f869568
infocinc <nicolas.dutil@infocinc.com>
2014-11-20 16:06:01 -0500

    add LocaleResource model

---
+++ models/LocaleResource.js
@@ -0,0 +1,17 @@
+var keystone = require('keystone'),
+	Types = keystone.Field.Types;
+
+var Resource = new keystone.List("LocaleResource", {nocreate: true, track: true});
+
+
+Resource.add({
+	_id : {type: String, required: true},
+	ns : {type: String, required:true},
+	lng: {type: String, required:true},
+	resource: {type: String, required: true},
+	timestamp: {type: Date, default: Date.now, format: "Do MMM YYYY, LT", noedit: true}
+});
+
+Resource.defaultSort = '-timestamp';
+Resource.defaultColumns = 'ns, lng|20%, timestamp';
+Resource.register();
