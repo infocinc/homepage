e079256a72d775f66656d9aed262031e09b9cb86
infocinc <nicolas.dutil@infocinc.com>
2014-12-13 04:52:32 -0500

    add locales update

---
+++ updates/0.0.2-locales.js
@@ -0,0 +1,36 @@
+var fs = require('fs'),
+	dsy = require('../lib/dsy');
+
+var namespaces = ['projects'],
+	lngs = ['fr', 'en'],
+	resources = [];
+
+
+
+function loadResource($ns,$lng) {
+	var data = fs.readFileSync('./locales/' + $lng +'/' + $ns + '.json', 'utf8');
+
+	return {
+		_id: $ns + '_' + $lng,
+		ns: $ns,
+		lng:  $lng,
+		resource: JSON.stringify(JSON.parse(data)) 
+	}
+}
+
+
+var tuples = [];
+// create tuples of ns, lng
+namespaces.forEach(function(ns) {
+	lngs.forEach(function(lng) {
+		resources.push(loadResource(ns,lng));
+	});
+});
+console.log(resources);
+
+exports.create = {
+	LocaleResource: resources
+};
+
+
+
