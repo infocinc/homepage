0a4cf187f41eca337033af8bd1f8b445f6ba7cd7
infocinc <nicolas.dutil@infocinc.com>
2014-12-15 04:19:11 -0500

    add minify js

---
+++ public/js/canvas-utils.js
@@ -0,0 +1,135 @@
+var canvas = $('canvas'),
+    images = [];
+
+function startAnimation(i, refresh) {
+    var c = canvas.get(i),
+        img = images[i],
+        ct = c.getContext('2d');
+
+    drawBackground(i, c, ct,0,0);
+    setTimeout(function() {
+        var startTime = (new Date()).getTime();
+        repaint(i, c, ct, startTime, refresh);
+    }, 10);
+}
+
+function loadImages(sourceSet, refresh) {
+    for (i = 0; i < sourceSet.length; i++) {
+        images[i] = new Image();
+        images[i].src = sourceSet[i];
+        images[i].onload = function(i) {
+            return function() { startAnimation(i, refresh);};
+        }(i);
+    }
+}
+
+function initScene(scene) {
+    resize(scene.container);
+    $(window).on('resize', function() {
+        resize(scene.container)
+    });
+    $(canvas.get(0)).css('background', scene.background);
+    loadImages(scene.sourceSet, scene.refresh);
+}
+
+///////////////////////////////////////////////
+// Resize canvas based upon container dimension
+///////////////////////////////////////////////
+function resize(tag) {
+    var h = parseInt($(tag).css('height'), 10),
+        w = $(window).width();
+
+    canvas.each(function(i) {
+        $(this).attr({
+            'width': w,
+            'height': h
+        });
+    });
+}
+
+///////////////////////////
+// Draw background 
+// Repeat pattern if necessary
+///////////////////////////
+
+function drawBackground(index,c,ct,tx,ty) {
+    var cw = c.width, ch = c.height,
+        img = images[index],
+        w = img.width,h = img.height,
+        x = tx, y = ty,
+        ix = 0, iy = 0, 
+        ry = 0, rx = 0, 
+        ih = h ,iw = w,
+        rw = w, rh = h, 
+        reset,resety,
+        paint = true,painty = true;
+    
+    while(painty) {
+        x = tx, ix= 0, iw = w, rw = w, rx = 0;
+        reset = false;
+        paint = true;
+        if (y + h > ch) {
+            ry = ih = ch - y;
+            rh = (y+h) - ch;
+        }
+        if (resety) {
+            if (y >= ty) {
+                painty = false;
+            } else if (y + ih > ty) {
+                ih = ty - y;
+            }
+        }
+        while(paint) {
+                if (x+w > cw) {
+                    rx = iw = cw - x;
+                    rw = (x+w) - cw;
+                }
+                if (reset) {
+                    if (x >= tx) {
+                        paint = false;
+                    } else if (x + iw > tx) {
+                        iw = tx - x;
+                    }
+                }
+                if (paint) {
+                    ct.drawImage(img,ix,iy,iw,ih,x,y,iw,ih);
+                    x = x + iw;
+                    iw = w;
+                    ix = 0;
+                }
+                if (x == cw) {
+                    reset = true;
+                    x = 0;
+                    ix = rx;
+                    iw = rw;
+                }
+        }
+        y = y + ih;
+        ih = h;
+        iy = 0;
+        if (y >= ch) {
+            resety = true;
+            y = 0;
+            iy = ry;
+            ih = rh;
+        }
+    }
+}
+
+
+
+//////////////////////////////////////
+// refresh canvas
+//////////////////////////////////////
+function repaint(index, c, ct, lastTime, refresh) {
+    var now = new Date().getTime();
+
+    ct.clearRect(0, 0, c.width, c.height);
+    if (typeof refresh === "function") {
+        refresh(index, c, ct, now,lastTime);
+    }
+
+    requestAnimFrame(function() {
+        repaint(index, c, ct, now,refresh);
+    });
+}
---
+++ public/js/scale.js
@@ -0,0 +1,90 @@
+// SCALING ANIMATION
+// LIMITED TO 1 CANVAS FOR NOW
+var scale = namespace('ifc.scale'),
+	canvas = $('canvas').get(0),
+    scaler = {};
+
+scale.init = function(options) {
+    var settings = {
+        tag: '#hero',
+    }
+
+    if (options) {
+        $.extend(settings, options);
+    }
+    scaler.img = new Image();
+    scaler.tag = settings.tag;
+    scaler.context = canvas.getContext('2d');
+    // resize canvas
+    $(window).on('resize', function() {
+        scale.resize(scale.refreshImage);
+    });
+
+    scale.resize(scale.refreshImage)
+
+    scaler.img.onload = function() {
+        scale.scaleImage(1, 0, 0);
+        setTimeout(function() {
+            scale.animate();
+        }, 10);
+    }
+
+}
+
+scale.refreshImage = function(w, h) {
+    var imgPath;
+
+    if (w > 1000) {
+        imgPath = '/images/home/bg-desktop.jpg';
+    } else if (w > 767) {
+        imgPath = '/images/home/bg-tablet.jpg';
+    } else {
+        imgPath = '/images/home/bg-mobile.jpg';
+    }
+    scaler.img.src = imgPath;
+}
+
+scale.resize = function(pre, post) {
+    var h = $(scaler.tag).height(),
+        w = $(window).width();
+
+    if (pre) {
+        pre(w, h);
+    }
+    canvas.width = w;
+    canvas.height = h;
+    if (post) {
+        post();
+    }
+}
+
+scale.animate = function() {
+    var now = (new Date()).getTime(),
+    	sin = Math.sin(now/3000),
+        s = sin*0.10 + 0.90;
+
+    scaler.context.clearRect(0, 0, canvas.width, canvas.height);
+    scale.scaleImage(s,0,0);
+
+    requestAnimFrame(function() {
+        scale.animate();
+    });
+}
+
+
+scale.scaleImage = function(s, tx, ty) {
+    var cw = canvas.width,
+        ch = canvas.height,
+        w = scaler.img.width,
+        h = scaler.img.height,
+        sw = s * w,
+        sh = s * 0.85 * h,
+        tw = (w - sw ) / 2,
+        th = (h - sh ) / 2;
+    
+    scaler.context.drawImage(scaler.img, tw, th, sw, sh, 0, 0, cw, ch);
+}
+
+$(document).ready(function() {
+    scale.init();
+});
